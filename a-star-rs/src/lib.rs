5758e31b961a6e2f364b021106918fd768d2434c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-10 20:38:02 +0200

    create nodes when call node constructor

--- a-star-rs/src/lib.rs
+++ a-star-rs/src/lib.rs
@@ -2,9 +2,28 @@
 #[allow(dead_code)]
 mod lib {
 
+    pub struct Node {
+        heuristic: u8,
+    }
+
+    impl Node {
+
+        /// Constructor for a new Node object.
+        ///
+        /// # Returns:
+        ///
+        /// Created Node object.
+        pub fn new() -> Node {
+            Node {
+                heuristic: 0,
+            }
+        }
+    }
+
     pub struct Nodes {
         width: u8,
         height: u8,
+        nodes: Vec<Node>,
     }
 
     impl Nodes {
@@ -23,9 +42,18 @@ mod lib {
             width: u8,
             height: u8,
         ) -> Nodes {
+
+            let mut nodes: Vec<Node> = Vec::new();
+            let indices = width * height;
+
+            for _ in 0..indices {
+                nodes.push(Node::new());
+            }
+
             Nodes {
                 width: width,
                 height: height,
+                nodes: nodes,
             }
         }
     }
