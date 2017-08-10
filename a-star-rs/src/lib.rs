6509a2c89c2f5233b493d20928debb111381f70e
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-10 14:04:28 +0200

    integrate constructor for nodes object creation

--- a-star-rs/src/lib.rs
+++ a-star-rs/src/lib.rs
@@ -3,6 +3,31 @@
 mod lib {
 
     pub struct Nodes {
+        width: u8,
+        height: u8,
+    }
+
+    impl Nodes {
+
+        /// Constructor for a new Nodes object.
+        ///
+        /// # Arguments:
+        ///
+        /// * `width` - the number of nodes per line,
+        /// * `height` - the number of lines
+        ///
+        /// # Returns:
+        ///
+        /// Created Nodes object.
+        pub fn new(
+            width: u8,
+            height: u8,
+        ) -> Nodes {
+            Nodes {
+                width: width,
+                height: height,
+            }
+        }
     }
 }
 
