9d922eb1efbea8d8882c0282ffe642af00a101d5
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-10-29 11:33:57 +0100

    integrate a method to be called from other modules

--- a-star-rs/src/lib.rs
+++ a-star-rs/src/lib.rs
@@ -6,5 +6,36 @@ mod node;
 /// Contains all nodes.
 mod nodes;
 
+/// Main interface that returns the path.
+///
+/// # Args:
+///
+/// * `width` - the number of nodes per line (max allowed: 10),
+/// * `height` - the number of nodes per column (max allowed: 10),
+/// * `departure` - the departure node index,
+/// * `arrival` - the arrival node index
+#[no_mangle]
+pub fn get_path(
+    path: &mut [u8],
+    width: u8,
+    height: u8,
+    departure: u8,
+    arrival: u8,
+) {
+
+    let mut nodes = nodes::Nodes::new(
+        width,
+        height,
+        departure as usize,
+        arrival as usize,
+    );
+
+    let path_indices = nodes.research_path();
+
+    for (counter, index) in path_indices.iter().enumerate() {
+        path[counter] = *index as u8;
+    }
+}
+
 #[cfg(test)]
 mod tests;
